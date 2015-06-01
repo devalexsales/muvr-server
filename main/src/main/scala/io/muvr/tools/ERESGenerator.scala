d1fad983664a32da9d272c89268c83243d441d15
Jan Machacek <jan.machacek@gmail.com>
2015-06-01 19:12:09 +0100

    generator

--- main/src/main/scala/io/muvr/tools/ERESGenerator.scala
+++ main/src/main/scala/io/muvr/tools/ERESGenerator.scala
@@ -2,4 +2,33 @@ package io.muvr.tools
 
 class ERESGenerator() {
 
+  /*
+  {
+  "id":"968350C6-7765-4869-AC3C-B0FF5D31D7BD",
+  "session":{
+    "startDate":"2015-06-01T17:58:35+0000",
+    "muscleGroupIds":["arms"],
+    "intendedIntensity":0.8,
+    "title":"Demo 1"
+  },
+  "sets":[
+    {"sets":[{"exercise":"arms/dumbbell-bicep-curl", "confidence":1, "repetitions":10, "weight":20, "intensity":0.5}]},
+    {"sets":[{"exercise":"arms/cable-tricep-extension", "confidence":1, "repetitions":10, "weight":20, "intensity":0.5}]},
+
+    {"sets":[{"exercise":"arms/dumbbell-bicep-curl", "confidence":1, "repetitions":10, "weight":22.5, "intensity":0.6}]},
+    {"sets":[{"exercise":"arms/cable-tricep-extension", "confidence":1, "repetitions":10, "weight":22.5, "intensity":0.5}]},
+
+    {"sets":[{"exercise":"arms/dumbbell-bicep-curl", "confidence":1, "repetitions":10, "weight":25, "intensity":0.7}]},
+    {"sets":[{"exercise":"arms/cable-tricep-extension", "confidence":1, "repetitions":10, "weight":25, "intensity":0.7}]},
+
+    {"sets":[{"exercise":"arms/dumbbell-bicep-curl", "confidence":1, "repetitions":7, "weight":27.5, "intensity":0.85}]},
+    {"sets":[{"exercise":"arms/cable-tricep-extension", "confidence":1, "repetitions":7, "weight":27.5, "intensity":0.85}]},
+
+    {"sets":[{"exercise":"arms/dumbbell-bicep-curl", "confidence":1, "repetitions":3, "weight":30, "intensity":0.95}]},
+    {"sets":[{"exercise":"arms/cable-tricep-extension", "confidence":1, "repetitions":4, "weight":30, "intensity":0.95}]}
+  ],
+  "examples":[],
+  "deviations":[]
+}
+   */
 }
